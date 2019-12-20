52e512a98d68e65a33df87af14a4ed870218c70c
Paul Melero <paulmelero@gmail.com>
2019-12-20 18:43:25 +0100

    changed to mailgun

--- functions/send-alert.js
+++ functions/send-alert.js
@@ -1,7 +1,8 @@
-// with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
-const sendMail = require('sendmail')();
-
-const { homepage } = require('../package.json');
+const mailgun = require('mailgun-js');
+const {
+  env: { DOMAIN, MAILGUN_API_KEY }
+} = process;
+const mg = mailgun({ apiKey: MAILGUN_API_KEY, domain: DOMAIN });
 
 exports.handler = (_event, _context, callback) => {
   if (!process.env.CONTACT_EMAIL) {
@@ -11,23 +12,25 @@ exports.handler = (_event, _context, callback) => {
     });
   }
 
+  const homepage = 'static-uptime-robot.netlify.com';
+
   const descriptor = {
-    from: `"Robot" <no-reply@static-uptime-robot.netlify.com>`,
+    from: `"Robot" <no-reply@${DOMAIN}>`,
     to: process.env.CONTACT_EMAIL,
     subject: `Site ${process.env.TARGET_SITE} is down at the moment!`,
     html: `😱 Check out the status page on <a href="${homepage}">${homepage}</a>`
   };
 
-  sendMail(descriptor, e => {
-    if (e) {
+  mg.messages().send(descriptor, function(error, body) {
+    if (error) {
       callback(null, {
         statusCode: 500,
-        body: e.message
+        body: error.message
       });
     } else {
       callback(null, {
         statusCode: 200,
-        body: 'Mail sent 🎯'
+        body: 'Mail sent 🎯 \n' + body
       });
     }
   });
